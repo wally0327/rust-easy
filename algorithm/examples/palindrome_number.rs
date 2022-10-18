39c723c1a872767c54b6e12b2045b363b75d3b23
wally0327 <wallywxy@gmail.com>
2022-10-18 18:22:25 +0800

    feat: solve palindrome_number problem

---
+++ algorithm/examples/palindrome_number.rs
@@ -0,0 +1,42 @@
+impl Solution {
+    // 构建反向数字
+    pub fn is_palindrome(x: i32) -> bool {
+        let res = if x < 0 {
+            false
+        } else {
+            let mut x2 = 0;
+            let mut y = x;
+            while y > 0 {
+                x2 = x2 * 10 + y % 10;
+                y = y / 10;
+            }
+            println!("x: {}, y:{}", x, x2);
+            x == x2
+        };
+        return res;
+    }
+
+    // 直接使用字符串头尾比对
+    pub fn is_palindrome_2(x: i32) -> bool {
+        let str_num = x.to_string();
+        let mut i = 0;
+        let mut j = str_num.len() - 1;
+
+        while i < j {
+            if str_num.get(i..i + 1).unwrap() != str_num.get(j..j + 1).unwrap() {
+                return false;
+            }
+            i = i + 1;
+            j = j - 1;
+        }
+
+        return true;
+    }
+}
+
+struct Solution {}
+
+fn main() {
+    let num = -121;
+    println!("{:?}", Solution::is_palindrome(num));
+}
